e90c66cfb2a9e056dcb13868b21ba288a998cefc
jaqusa <dev@javierquinte.com>
2018-12-12 17:53:47 -0600

    done

---
+++ starter-code/src/components/ProductTable.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+const ProductTable = () => {
+  return (
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+    </table>
+  );
+};
+
+export default ProductTable;
